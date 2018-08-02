a926b1670ea10bda8f658e81ff5098f080ecbf3d
tien <lqtien@apcs.vn>
2018-08-02 14:18:10 +0700

    rhc

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var apiv1 = require('./api');
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 apiv1.setUp(app);
 
-app.listen(5000, function() {
-  console.log('%s listening at %s', app.name, app.url);
+app.listen(server_port, server_ip_address, function() {
+  console.log('Listening on ' + server_ip_address + ', port ' + server_port);
 });
